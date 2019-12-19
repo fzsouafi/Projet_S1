fe64147696352a86e37731b93f80510d3b05c37b
SOUAFI <fatima-zohra.souafi@etu.univ-lyon1.fr>
2019-12-19 16:00:01 +0100

    serve.py bonne version

--- serving_static/serve.py
+++ serving_static/serve.py
@@ -26,10 +26,10 @@ def create_entry():
             value[0] = req['namaesp']
         taxonomic_dico = findTaxID(wikipedia)
         taxonomic_dico[req['scientiesp']].append(req['catesp'])
-        file.write('\n'+s + taxonomic_dico[req['scientiesp']][0] + s + v + s + taxonomic_dico[req['scientiesp']][5] + s + v + s
+        file.write(s + taxonomic_dico[req['scientiesp']][0] + s + v + s + taxonomic_dico[req['scientiesp']][5] + s + v + s
                    + taxonomic_dico[req['scientiesp']][1] + s + v + s + taxonomic_dico[req['scientiesp']][2]
                    + s + v + s + taxonomic_dico[req['scientiesp']][3] + s + v + s + taxonomic_dico[req['scientiesp']][4]
-                   + s + v + s + " https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+taxonomic_dico[req['scientiesp']][4] + s + v + s + taxonomic_dico[req['scientiesp']][0]+'"')
+                   + s + v + s + " https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+taxonomic_dico[req['scientiesp']][4] + s + v + s + taxonomic_dico[req['scientiesp']][0]+'"' + '\n')
         file.close()
         createIndex()
         res = make_response(jsonify(taxonomic_dico[req['scientiesp']]), 200)
@@ -38,10 +38,10 @@ def create_entry():
         scientific_name = findLatin_name(req['namaesp'])
         taxonomic_dico = findTaxID(scientific_name)
         taxonomic_dico[req['namaesp']].append(req['catesp'])
-        file.write( '\n'+s + taxonomic_dico[req['namaesp']][0] + s + v + s + taxonomic_dico[req['namaesp']][5] + s + v + s
+        file.write( s + taxonomic_dico[req['namaesp']][0] + s + v + s + taxonomic_dico[req['namaesp']][5] + s + v + s
                     + taxonomic_dico[req['namaesp']][1] + s + v + s + taxonomic_dico[req['namaesp']][2]
                     + s + v + s + taxonomic_dico[req['namaesp']][3] + s + v + s + taxonomic_dico[req['namaesp']][4] + s + v + s
-                    + " https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+taxonomic_dico[req['namaesp']][4] + s + v + s + taxonomic_dico[req['namaesp']][0]+'"')
+                    + " https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+taxonomic_dico[req['namaesp']][4] + s + v + s + taxonomic_dico[req['namaesp']][0]+'"'+ '\n')
         file.close()
         createIndex()
         res = make_response(jsonify(taxonomic_dico[req['namaesp']]), 200)
@@ -78,4 +78,86 @@ def createIndex():
 if __name__ == "__main__":
     createIndex()
     app.run(debug=True)
+
+	# serve.py
+import subprocess
+from flask import Flask
+from flask import render_template, jsonify, current_app, request, make_response, flash
+from function_search_taxonomy import *
+import sys
+sys.path.insert(1,"static/")
+from MonPotager import main
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+
+# routes dans lesquelles est affiché un message dans la console ( message d'erreur ou alors le dictionnaire contenant les informations de l'espèce)
+
+@app.route("/species/new-entry", methods=["GET", "POST"])
+def create_entry():
+
+    req = request.get_json()
+    file = open("static/database/especes_v2.csv", "a")
+    s = "\""
+    v = ","
+    if req['scientiesp'] != "-":
+        wikipedia = findLatin_name(req['scientiesp'])
+        for value in wikipedia.values():
+            value[0] = req['namaesp']
+        taxonomic_dico = findTaxID(wikipedia)
+        taxonomic_dico[req['scientiesp']].append(req['catesp'])
+        file.write(s + taxonomic_dico[req['scientiesp']][0] + s + v + s + taxonomic_dico[req['scientiesp']][5] + s + v + s
+                   + taxonomic_dico[req['scientiesp']][1] + s + v + s + taxonomic_dico[req['scientiesp']][2]
+                   + s + v + s + taxonomic_dico[req['scientiesp']][3] + s + v + s + taxonomic_dico[req['scientiesp']][4]
+                   + s + v + s + " https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+taxonomic_dico[req['scientiesp']][4] + s + v + s + taxonomic_dico[req['scientiesp']][0]+'"' + '\n')
+        file.close()
+        createIndex()
+        res = make_response(jsonify(taxonomic_dico[req['scientiesp']]), 200)
+        return res
+    else:
+        scientific_name = findLatin_name(req['namaesp'])
+        taxonomic_dico = findTaxID(scientific_name)
+        taxonomic_dico[req['namaesp']].append(req['catesp'])
+        file.write( s + taxonomic_dico[req['namaesp']][0] + s + v + s + taxonomic_dico[req['namaesp']][5] + s + v + s
+                    + taxonomic_dico[req['namaesp']][1] + s + v + s + taxonomic_dico[req['namaesp']][2]
+                    + s + v + s + taxonomic_dico[req['namaesp']][3] + s + v + s + taxonomic_dico[req['namaesp']][4] + s + v + s
+                    + " https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+taxonomic_dico[req['namaesp']][4] + s + v + s + taxonomic_dico[req['namaesp']][0]+'"'+ '\n')
+        file.close()
+        createIndex()
+        res = make_response(jsonify(taxonomic_dico[req['namaesp']]), 200)
+        return res
+
+
+
+
+@app.route('/association/new-entry', methods=["POST", "GET"])
+def create_association():
+    req = request.get_json()
+    print(req)
+    file = open("static/database/associations.csv","a")
+    s = "\""
+    v = ","
+    file.write(s + req["espSource"] + s + v + s + req["espInteraction"] + s + v + s + req["espCible"] + s + v + v + v + "\n")
+    file.close()
+    createIndex()
+    return req
+
+
+
+# les fonctions de base du serveur et une fonction d'execution automatique de Monpotager.py
+
+@app.route("/")
+def monpotager():
+    return render_template('index.html')
+
+def createIndex():
+    main()
+    subprocess.run(["mv","index.html","templates/"])
+
+# run the application
+if __name__ == "__main__":
+    createIndex()
+    app.run(debug=True)
+
 
